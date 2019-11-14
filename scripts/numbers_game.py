43071736ace3dd043853a2bab02419099c03ee63
ufengger <haifeng.wu@foxmail.com>
2019-11-14 16:40:45 +0800

    clean

--- scripts/numbers_game.py
+++ scripts/numbers_game.py
@@ -35,7 +35,7 @@ class Hypothesis:
     def __init__(self, predicate, name):
         self.predicate = predicate
         self.name = name
-        
+
         # The size of the extension of this hypothesis.
         self.size = sum(1 for n in number_range if self.predicate(n))
 
@@ -104,11 +104,11 @@ class Hypothesis:
     # to the first number in the range of numbers, etc.
     def get_extension_indication(self):
         return [int(self.predicate(x)) for x in number_range]
-    
+
     # Returns the extension of the hypothesis as a list of numbers in the extension
     def get_extension(self):
         return [x for x in number_range if self.predicate(x)]
-    
+
     # The probability under this hypothesis of generating the given number.
     def prob(self, number):
         return int(self.predicate(number)) / self.size
@@ -131,7 +131,7 @@ class Bayes:
     def posterior(prior, likelihood):
         post = prior * likelihood
         return post / sum(post)
-    
+
     # Returns the set of marginal probabilities p(x in C|D) for each x as a vector whose index
     # positions correspond to those of the number_range of x. Note: this is not a probability
     # distribution but a set of marginal probabilities.
@@ -139,11 +139,11 @@ class Bayes:
     def posterior_concept_membership_marginals(hypotheses, posterior):
         # Get the concept membership indicators (0 or 1) for each number x and hypothesis h as a matrix.
         inconcept = np.array(np.column_stack([h.get_extension_indication() for h in hypotheses]))
-        
+
         # Matrix-multiply by the posterior p(h|D) vector on the hypotheses to get the Bayesian
         # Model Average of these indicators, which will equate to p(x in C|D).
         return np.matmul(inconcept, posterior)
-    
+
 class Graphing:
     @staticmethod
     def plot_hypothesis_space(hypotheses, filename):
@@ -151,7 +151,7 @@ class Graphing:
         # range (indexed by the first axis) in the extension of each hypotheses (indexed by the
         # second axis).
         image = np.column_stack([hypothesis.get_extension_indication() for hypothesis in hypotheses])
-        
+
         plt.gray()
         plt.xticks([i for i in range(len(hypotheses))], [hypothesis.name for hypothesis in hypotheses], rotation='vertical')
         plt.yticks([0, 20, 40, 60, 80, 100])
@@ -159,18 +159,18 @@ class Graphing:
         plt.ylim(-1, 100)
         plt.imshow(image, aspect='auto', interpolation='none', origin='lower')
         save_fig(filename)
-        
+
         # Don't block, continue to show the next figure.
         plt.show(block=False)
-        
+
     @staticmethod
     def plot_prior_lik_post(data, hypotheses, prior, likelihood, posterior, filename):
         plt.figure(figsize=(12, 10))
         plt.suptitle('data = {' + ','.join([str(x) for x in data]) + '}', fontsize=24, x=0.56, y=1.03)
-    
+
         # sub-plot title y-alignment
         y_title_align = -0.07
-    
+
         # Plot the prior.
         plt.subplot(1, 3, 1)
         plt.title('prior', y=y_title_align)
@@ -178,7 +178,7 @@ class Graphing:
                  [prob for prob in prior[::-1]], color='blue')
         plt.yticks([i for i in range(len(hypotheses))],
                    [hypothesis.name for hypothesis in hypotheses[::-1]])
-        
+
         # Plot the likelihood.
         plt.subplot(1, 3, 2)
         plt.title('lik', y=y_title_align)
@@ -192,7 +192,7 @@ class Graphing:
             labelleft=False)
         # The maximum likelihood can be small. Use scientific notation.
         plt.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
-                
+
         # Plot the posterior.
         plt.subplot(1, 3, 3)
         plt.title('post', y=y_title_align)
@@ -204,9 +204,9 @@ class Graphing:
             left=False,
             right=False,
             labelleft=False)
-        
+
         save_fig(filename)
-        
+
         # Don't block, continue to show the next figure.
         plt.show(block=False)
 
@@ -214,11 +214,11 @@ class Graphing:
     def plot_post_membership_marginals(data, hypotheses, posterior, marginals, filename):
         # Make way for 4 plots arranged in a suitable 2x2 grid.
         figure, axes = plt.subplots(2, 2, figsize=(12, 8), gridspec_kw={'width_ratios':[7, 1], 'height_ratios':[2, 5]}, )
-        
+
         # x-axis limits for those subsplots with the number range on their x-axis.
         x_min = number_range[0] - 1
         x_max = number_range[-1] + 1
-        
+
         # Plot the posterior membership marginals in the top-left slot.
         ax_marginals = axes[0, 0]
         ax_marginals.bar([x for x in number_range],
@@ -229,16 +229,16 @@ class Graphing:
         # Hide spines.
         ax_marginals.spines['top'].set_visible(False)
         ax_marginals.spines['right'].set_visible(False)
-        
+
         # Rank the hypotheses from least to most probable and keep the 10 most probable only.
         top_n = 10
         top_10_ranked_hypotheses_posterior = np.row_stack([[hypotheses[i], posterior[i]] for i in range(len(hypotheses))])
         top_10_ranked_hypotheses_posterior = sorted(top_10_ranked_hypotheses_posterior, key=lambda row: row[1])
         top_10_ranked_hypotheses_posterior = top_10_ranked_hypotheses_posterior[-top_n:]
 
-        # Calculate scatter points for the hypotheses extensions subplot. 
+        # Calculate scatter points for the hypotheses extensions subplot.
         extension_points = [[x, i] for i in range(top_n) for x in top_10_ranked_hypotheses_posterior[i][0].get_extension()]
-        
+
         # Plot the hypotheses extensions in the lower-left slot.
         ax_extensions = axes[1, 0]
         ax_extensions.set_ylim(0, top_n-1)
@@ -275,8 +275,8 @@ class Graphing:
         ax_extensions.spines['right'].set_visible(False)
         # Prevent clipping of scatter points at subplot boundaries using clip_on=False.
         ax_extensions.scatter([point[0] for point in extension_points], [point[1] for point in extension_points], color='blue', clip_on=False)
-            
-        # Calculate plot points for the posterior distribution subplot. 
+
+        # Calculate plot points for the posterior distribution subplot.
         ax_posterior = axes[1, 1]
         # Prevent clipping of scatter points at subplot boundaries using clip_on=False.
         ax_posterior.plot([top_10_ranked_hypotheses_posterior[i][1] for i in range(top_n)],
@@ -292,13 +292,13 @@ class Graphing:
             right=False,       # ticks along the right edge are off
             labelleft=False)   # labels along the left edge are off
         ax_posterior.set_title(r'$p(h|{})$'.format(','.join([str(x) for x in data])), y=-0.12)
-        
+
         # Hide the axes of the plot in the top-right slot.
         axes[0, 1].axis('off')
-        
+
         save_fig(filename)
         plt.show()
-    
+
 # The hypothesis space: a vector of hypotheses.
 hypotheses = Hypothesis.even_odd() \
         + [Hypothesis(lambda x: x == np.floor(0.5 + np.sqrt(x)) ** 2, 'squares')] \
@@ -308,11 +308,11 @@ hypotheses = Hypothesis.even_odd() \
         + [Hypothesis(lambda x: True, 'all')] \
         + Hypothesis.compose(Hypothesis.powers([2]), Hypothesis.explicit_set([37]), lambda x, y: x or y, ' +') \
         + Hypothesis.compose(Hypothesis.powers([2]), Hypothesis.explicit_set([32]), lambda x, y: x and not y, ' -')
-    
+
 # The prior on the hypothesis space (a 1-d vector).
 #{odd,even} = 0.5 ; {powers of 2 + 37,powers of 2 - 32} = 0.001; else=0.1
 prior = np.concatenate([np.array([0.5, 0.5]), 0.1*np.ones(28), np.array([0.001, 0.001])])
-prior = prior / np.sum(prior)    
+prior = prior / np.sum(prior)
 
 # Calculations for D={16}
 data_16 = [16]
